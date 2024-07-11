e1f264f0e64fdf9df4a47ef1b6d10c3d89af0653
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-07-10 23:24:31 -0300

    feat: add h2Database, migrations and first entity

---
+++ src/main/java/com/br/planner/entity/Trip.java
@@ -0,0 +1,32 @@
+package com.br.planner.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+public class Trip {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String destination;
+
+    @Column(name = "starts_at", nullable = false)
+    private LocalDateTime startsAt;
+
+    @Column(name = "ends_at", nullable = false)
+    private LocalDateTime endsAt;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private boolean isConfirmed;
+
+    @Column(name = "owner_name", nullable = false)
+    private String ownerName;
+
+    @Column(name = "owner_email", nullable = false)
+    private String ownerEmail;
+}
