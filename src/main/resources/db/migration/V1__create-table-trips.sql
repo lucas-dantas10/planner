ef4d243056e203972ee6846d8da404ad27e92428
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-07-14 11:27:57 -0300

    fix: adjust in path of migrations

--- src/main/resources/db/migration/V1__create-table-trips.sql
+++ src/main/resources/db/migration/V1__create-table-trips.sql
@@ -1,7 +1,7 @@
 CREATE TABLE trips (
     id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
     destination VARCHAR(255) NOT NULL,
-    starts_at TIMESTAMP NOT NULL
+    starts_at TIMESTAMP NOT NULL,
     ends_at TIMESTAMP NOT NULL,
     is_confirmed BOOLEAN NOT NULL,
     owner_name VARCHAR(255) NOT NULL,
