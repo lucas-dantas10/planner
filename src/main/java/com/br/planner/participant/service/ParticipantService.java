344af992811f5a46ca03e811ff501bf80a0bf049
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-07-17 00:06:25 -0300

    feat: implemented participant service

--- src/main/java/com/br/planner/participant/service/ParticipantService.java
+++ src/main/java/com/br/planner/participant/service/ParticipantService.java
@@ -1,19 +1,55 @@
 package com.br.planner.participant.service;
 
+import com.br.planner.participant.dto.ParticipantCreateResponse;
+import com.br.planner.participant.dto.ParticipantRequestPayload;
+import com.br.planner.participant.entity.Participant;
+import com.br.planner.participant.repository.ParticipantRepository;
+import com.br.planner.trip.entity.Trip;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class ParticipantService implements ParticipantServiceInterface {
-    @Override
-    public void registerPartipantsToTrip(List<String> participants, UUID tripId) {
 
+
+    private final ParticipantRepository participantRepository;
+
+    public ParticipantService(ParticipantRepository participantRepository) {
+        this.participantRepository = participantRepository;
+    }
+
+    @Override
+    public void registerParticipantsToTrip(List<String> participants, Trip trip) {
+        log.info("Enviando email para os participants");
+
+        List<Participant> newParticipants = participants
+                .stream()
+                .map(email -> new Participant(email, trip))
+                .toList();
+        this.participantRepository.saveAll(newParticipants);
+
+        log.info("Emails enviados");
+    }
+
+    @Override
+    public ParticipantCreateResponse registerParticipantToTrip(ParticipantRequestPayload payload, Trip trip) {
+        Participant newParticipant = new Participant(payload.email(), payload.name(), trip);
+        this.participantRepository.save(newParticipant);
+
+        return new ParticipantCreateResponse(newParticipant.getId());
     }
 
     @Override
     public void triggerConfirmationEmailToParticipants(UUID tripId) {
+
+    }
+
+    @Override
+    public void triggerConfirmationEmailToParticipant(String email) {
 
     }
 }
--- src/main/java/com/br/planner/participant/service/ParticipantServiceInterface.java
+++ src/main/java/com/br/planner/participant/service/ParticipantServiceInterface.java
@@ -1,10 +1,16 @@
 package com.br.planner.participant.service;
 
+import com.br.planner.participant.dto.ParticipantCreateResponse;
+import com.br.planner.participant.dto.ParticipantRequestPayload;
+import com.br.planner.trip.entity.Trip;
+
 import java.util.List;
 import java.util.UUID;
 
 public interface ParticipantServiceInterface {
 
-    void registerPartipantsToTrip(List<String> participants, UUID tripId);
+    void registerParticipantsToTrip(List<String> participants, Trip trip);
+    ParticipantCreateResponse registerParticipantToTrip(ParticipantRequestPayload payload, Trip trip);
     void triggerConfirmationEmailToParticipants(UUID tripId);
+    void triggerConfirmationEmailToParticipant(String email);
 }
