70857f3329a1b0106d1be2775078701c1781e33d
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-07-18 11:56:50 -0300

    feat: create dto for response activity

---
+++ src/main/java/com/br/planner/activity/dto/ActivityRequestPayload.java
@@ -0,0 +1,6 @@
+package com.br.planner.activity.dto;
+
+import java.time.LocalDateTime;
+
+public record ActivityRequestPayload(String title, String occurs_at) {
+}
---
+++ src/main/java/com/br/planner/activity/dto/ActivityResponse.java
@@ -0,0 +1,6 @@
+package com.br.planner.activity.dto;
+
+import java.util.UUID;
+
+public record ActivityResponse(UUID activityId) {
+}
