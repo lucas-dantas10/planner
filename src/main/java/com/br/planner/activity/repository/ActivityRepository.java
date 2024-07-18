8aaa35415e0bdbc498cc24a1081127c04209bc2a
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-07-18 11:56:32 -0300

    feat: create repository

---
+++ src/main/java/com/br/planner/activity/repository/ActivityRepository.java
@@ -0,0 +1,11 @@
+package com.br.planner.activity.repository;
+
+import com.br.planner.activity.entity.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ActivityRepository extends JpaRepository<Activity, UUID> {
+    List<Activity> findByTripId(UUID tripId);
+}
