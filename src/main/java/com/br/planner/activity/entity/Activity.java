2dfd75b2b9ba47f2b5a3fd9153986e0f842a8d7b
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-07-18 11:22:12 -0300

    feat: create migration and entity for Activity

---
+++ src/main/java/com/br/planner/activity/entity/Activity.java
@@ -0,0 +1,34 @@
+package com.br.planner.activity.entity;
+
+import com.br.planner.trip.entity.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "activities")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(name = "occurs_at", nullable = false)
+    private LocalDateTime occursAt;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id")
+    private Trip trip;
+}
