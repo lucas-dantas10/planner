23db219bcc16ee6b365179befe1f9e8e15062984
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-07-20 20:32:55 -0300

    feat: create method to get all activities linked in trip

--- src/main/java/com/br/planner/activity/service/ActivityService.java
+++ src/main/java/com/br/planner/activity/service/ActivityService.java
@@ -1,11 +1,15 @@
 package com.br.planner.activity.service;
 
+import com.br.planner.activity.dto.ActivityData;
 import com.br.planner.activity.dto.ActivityRequestPayload;
 import com.br.planner.activity.dto.ActivityResponse;
 import com.br.planner.activity.entity.Activity;
 import com.br.planner.activity.repository.ActivityRepository;
 import com.br.planner.trip.entity.Trip;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ActivityService implements ActivityServiceInterface {
@@ -23,5 +27,13 @@ public class ActivityService implements ActivityServiceInterface {
         this.activityRepository.save(activity);
 
         return new ActivityResponse(activity.getId());
+    }
+
+    public List<ActivityData> getAllActivities(UUID tripId) {
+        return this.activityRepository
+                .findByTripId(tripId)
+                .stream()
+                .map(activity -> new ActivityData(activity.getId(), activity.getTitle(), activity.getOccursAt()))
+                .toList();
     }
 }
--- src/main/java/com/br/planner/activity/service/ActivityServiceInterface.java
+++ src/main/java/com/br/planner/activity/service/ActivityServiceInterface.java
@@ -1,10 +1,15 @@
 package com.br.planner.activity.service;
 
+import com.br.planner.activity.dto.ActivityData;
 import com.br.planner.activity.dto.ActivityRequestPayload;
 import com.br.planner.activity.dto.ActivityResponse;
 import com.br.planner.trip.entity.Trip;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface ActivityServiceInterface {
     ActivityResponse registerActivity(ActivityRequestPayload payload,
                                              Trip trip);
+    List<ActivityData> getAllActivities(UUID tripId);
 }
